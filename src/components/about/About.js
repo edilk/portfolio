1340861767c7337cc8642dce804c6d20c67f9fa7
edilk <ed2803k@gmail.com>
2023-06-16 21:06:12 +0600

    Footer added

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -84,6 +84,7 @@ export default function About() {
                 }
                 </ul>
             </div>
+            <hr />
         </section>
     );
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,20 @@
+import './footer.css';
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+
 export default function Footer() {
     return (
         <footer>
-            
+            <section>
+                <ul>
+                    <li><a href='https://github.com/edilk/'><FaGithub /></a></li>
+                    <li><a href='https://www.linkedin.com/in/edil-kamchybekov-bb8526234/'><FaLinkedinIn /></a></li>
+                    <li><a href='https://twitter.com/EKamchybekov'><FaTwitter /></a></li>
+                    <li><a href='https://www.instagram.com/kamchybekov28/'><FaInstagram /></a></li>
+                    <li><a href='https://www.facebook.com/edil.kamchybekov'><FaFacebook /></a></li>
+                </ul>
+                <hr />
+                <p>&copy; 2023 - Edil Kamchybekov</p>
+            </section>
         </footer>
     );
 }
