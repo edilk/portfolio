4781b964efd48989db9fa14c736e23742db3cd9a
edilk <ed2803k@gmail.com>
2023-06-16 20:28:33 +0600

    Contact fixed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,3 @@
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import './footer.css';
-
 export default function Footer() {
     return (
         <footer>
